d210fb89e8e1c638b53f2f52d7e17b5dbbb6bc32
Aymen Neffar <aymenneffar111@gmail.com>
2023-12-28 16:43:08 +0100

    pagination

--- src/components/StudentsList.jsx
+++ src/components/StudentsList.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './StudentsList.css';
+import './StudentsList.css'
 
-const StudentList = () => {
+const StudentsList = () => {
     const [students, setStudents] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [pageSize, setPageSize] = useState(5);
 
     useEffect(() => {
-        axios.get('http://localhost:8080/api/tp1/students')
+        axios.get(`http://localhost:8080/api/tp1/students?page=${currentPage}&size=${pageSize}`)
             .then(response => {
                 setStudents(response.data);
             })
             .catch(error => {
                 console.error('Il y a eu un problème avec la requête Axios', error);
             });
-    }, []);
+    }, [currentPage, pageSize]);
 
     return (
         <div>
@@ -23,8 +25,15 @@ const StudentList = () => {
                     <li key={student.id}>{student.name} - {student.city}</li>
                 ))}
             </ul>
+            <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0}>Précédent</button>
+            <button onClick={() => setCurrentPage(currentPage + 1)}>Suivant</button>
+            <select value={pageSize} onChange={(e) => setPageSize(e.target.value)}>
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+            </select>
         </div>
     );
 };
 
-export default StudentList;
+export default StudentsList;
